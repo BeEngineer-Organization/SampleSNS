3f56251491863e92640b967fc783682d82ad491b
EbuchiTsukasa <tsukasa_ebuchi@demia.co.jp>
2024-04-01 15:48:39 +0900

    requirements.txt, post_list.htmlの追加

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def post_list(request):
+    return render(request, "post_list.html")
